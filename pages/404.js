92fe9232cc48f05f2a8a80fe24a3aa63576ee36e
Amanda Tong <amanda.g.tong@gmail.com>
2022-12-16 19:01:21 -0500

    404 styling

--- pages/404.js
+++ pages/404.js
@@ -1,5 +1,8 @@
 export default function Oops(){
     return(
-        <div>not found</div>
+        <div style={{display:'flex', flexDirection: 'column', justifyContent:'center', alignItems:'center', height:'100vh', width: '100%'}}>
+            <img src="/assets/undraw_void.svg" style={{width:'30vmax', marginBottom: '2rem'}}/>
+            <div>Sorry, it doesn't look like that page exists.</div>
+            </div>
     )
 }
