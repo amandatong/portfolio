efef16c683a172ed2f576d7ee34102fa31c17e77
Amanda Tong <amanda.g.tong@gmail.com>
2022-12-22 15:32:26 -0500

    analytics and seo

--- pages/_app.js
+++ pages/_app.js
@@ -7,23 +7,34 @@ import Layout from '../components/Layout';
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import BasicCursor from '../components/BasicCursor';
+import Script from 'next/script'
 
-const base_targets = ['.menu','#lang div', '#links a', '#nav-menu a', '#right-bar a']
-const home_targets = ['.read-more', '.stamp', '.featured', '#postcard', ]
-const work_targets = ['#work a.linkable', '.case-links a','.work-nav a', '.scroll-to-top', '.button-link']
+// const base_targets = ['.menu','#lang div', '#links a', '#nav-menu a', '#right-bar a']
+// const home_targets = ['.read-more', '.stamp', '.featured', '#postcard', ]
+// const work_targets = ['#work a.linkable', '.case-links a','.work-nav a', '.scroll-to-top', '.button-link']
 
-const targets = base_targets.concat(home_targets, work_targets)
+// const targets = base_targets.concat(home_targets, work_targets)
 
 function App({ Component, pageProps, router }) {
   return (
     <>
         <Head>
             <title>Amanda Tong</title>
-            <meta name="description" content="Amanda Tong" />
+            <meta name="description" content="Hi! I'm Amanda Tong, a designer and developer." />
             <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
             <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
             <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
             <link rel="manifest" href="/site.webmanifest"/>
+            <Script async src="https://www.googletagmanager.com/gtag/js?id=G-T24N3Y39C4"/>
+            <Script id="google-analytics">
+                {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', 'G-T24N3Y39C4');
+                `}
+            </Script>
         </Head>
         {/* <Cursor targets={targets}/> */}
         <Layout router={router}>
